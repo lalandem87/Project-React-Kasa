4db600402398ac1918b41e42bf728bd26a9ad732
= <=>
2026-01-31 12:50:38 +0100

    add Error404 page

--- my-react-app/src/pages/Error404.jsx
+++ my-react-app/src/pages/Error404.jsx
@@ -1,3 +1,16 @@
-export default function Error404(){
-    
+import { NavLink } from "react-router-dom";
+import "./Error404.scss";
+
+export default function Error404() {
+  return (
+    <div className="error404">
+      <h1 className="error404__h1">404</h1>
+      <p className="error404__p">
+        Oups! La page que vous demandez n'existe pas.
+      </p>
+      <NavLink to="/" className="error404__navlink">
+        Retourner sur la page d'accueil
+      </NavLink>
+    </div>
+  );
 }
